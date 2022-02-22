8e58b909103df810d79f3ac5a51cc78aa3e311fd
Ivaylo Zhelev <ivaylo.zhelev@mypos.com>
2022-02-22 18:24:57 +0200

    Initial Commit

---
+++ ReviewRestaurants/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReviewRestaurants
+//
+//  Created by Ivaylo Zhelev on 22.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
